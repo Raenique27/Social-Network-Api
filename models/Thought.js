26416933f261b7ee38bccf78b06424203821837b
Raenique27 <raeniquewalters@gmail.com>
2022-11-10 10:39:33 -0500

    added getters to thought model

--- models/Thought.js
+++ models/Thought.js
@@ -17,7 +17,7 @@ const ReactionSchema = new Schema ({
     createdAt: {
         type: Date,
         default: Date.now,
-        
+        get: (createdAtVal) => dateFormat(createdAtVal)
     }
 })
 const ThoughtSchema = new Schema ({
@@ -30,7 +30,7 @@ const ThoughtSchema = new Schema ({
     createdAt: {
         type: Date,
         default: Date.now,
-
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     username: {
         type: String,
