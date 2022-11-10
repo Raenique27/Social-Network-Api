95dfeab9fb74d6fd7aed5c058214a4f9244c0021
Raenique27 <raeniquewalters@gmail.com>
2022-11-10 11:20:58 -0500

    added functionality to delete a user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -53,6 +53,19 @@ const userController = {
             })
             .catch(err => res.status(400).json(err));
     },
-    
 
-}
+    // delete user
+    deleteUser({params} , res) {
+        User.findOneAndDelete({_id: params.id})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({message: 'No user found with that id !'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = userController;
