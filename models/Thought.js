efe818e200a8a3e0d66a635c432fa21f0a824923
Raenique27 <raeniquewalters@gmail.com>
2022-11-10 10:29:15 -0500

    created ReactionSchema inside Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,25 @@
 const {Schema, model} = ('mongoose');
 
+const ReactionSchema = new Schema ({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        
+    }
+})
 const ThoughtSchema = new Schema ({
     thoughtText: {
         type: String,
