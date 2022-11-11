159daa43ff71d75e44806e1f7051f45411c94dbb
Raenique27 <raeniquewalters@gmail.com>
2022-11-11 11:18:53 -0500

    created functionality to create a thought anf update a thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -30,4 +30,41 @@ const thoughController = {
                 res.status(400).json(err);
             });
     },
+
+    // Create a thought
+    createThought({params, body}, res) {
+        Thought.create(body)
+            .then(({_id}) => {
+                return User.findOneAndUpdate(
+                    {_id: params.userId},
+                    {$push: {thoughts: _id}},
+                    {new: true}
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({message: 'No user found with this id'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // Update thought by id
+    updateThought({params, body}, res) {
+        Thought.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({message: 'No though found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    
+
+
 }
