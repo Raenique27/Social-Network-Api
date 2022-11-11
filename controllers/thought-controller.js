fc2e891bc0bea3899fc8c85cf208459bdf895852
Raenique27 <raeniquewalters@gmail.com>
2022-11-11 11:51:26 -0500

    added functionality to add a reaction to thought and remove a reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,6 +1,6 @@
 const {Thought, User} = require('../models');
 
-const thoughController = {
+const thoughtController = {
     // GET all thoughts
     getAllThoughts(req, res) {
         Thought.find()
@@ -64,7 +64,45 @@ const thoughController = {
             .catch(err => res.status(400).json(err));
     },
 
-    
+    deleteThought({params}, res) {
+        Thought.findOneAndDelete({_id: params.id})
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({message: 'No thought found with this id!'});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
 
+    // Add a reaction to a thought
+    addReaction({params, body}, res) {
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId},
+            {$push: {reactions: body}},
+            {new: true, runValidators: true}
+        )
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({message: 'No user found with this id'});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.json(err));
+    },
 
-}
+    // Remove reaction from thought
+    removeReaction({params}, res) {
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId},
+            {$pull: {reactions: {reactionId: params.reactionId}}},
+            {new: true}
+        )
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.json(err));
+    }
+};
+
+module.exports = thoughtController;
