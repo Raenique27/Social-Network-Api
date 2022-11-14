dfcca8839ec8917e506c7787766e78ebe8c0cabc
Raenique27 <raeniquewalters@gmail.com>
2022-11-14 09:27:33 -0500

    created index.js file for api routes and updated Thought model

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -65,6 +65,16 @@ const userController = {
                 res.json(dbUserData);
             })
             .catch(err => res.status(400).json(err));
+    },
+
+    // Add friend
+    addFriend({params}, res) {
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            {$push: {friends: params.friendId}},
+            {new: true}
+        )
+            .then
     }
 };
 
--- models/Thought.js
+++ models/Thought.js
@@ -1,26 +1,28 @@
-const {Schema, model} = ('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const ReactionSchema = new Schema ({
-    reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId
-    },
-    reactionBody: {
-        type: String,
-        required: true,
-        maxLength: 280
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => dateFormat(createdAtVal)
-    }
-})
-const ThoughtSchema = new Schema ({
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        }
+    })
+const ThoughtSchema = new Schema({
     thoughtText: {
         type: String,
         required: true,
@@ -39,14 +41,14 @@ const ThoughtSchema = new Schema ({
     // use ReactionSchema to validate data for reactions
     reactions: [ReactionSchema]
 },
-{
-    toJSON: {
-        virtuals: true
-    },
-    id: false
-});
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    });
 
-ThoughtSchema.virtual('reactionCount').get(function() {
+ThoughtSchema.virtual('reactionCount').get(function () {
     return this.reaction.length;
 });
 
